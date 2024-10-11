c7906414a470a0cb9bd39ce0f1c07cf4f0ed89b4
VSS <sandybreak3405@gmail.com>
2024-10-11 15:47:09 +0300

    ADD generate_dalle_img.py router

--- src/routers/commands/generate_dalle_img.py
+++ src/routers/commands/generate_dalle_img.py
@@ -5,6 +5,7 @@ import base64
 from aiogram.types import Message, ReplyKeyboardRemove, FSInputFile
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
+from aiogram.enums import ParseMode
 from aiogram import Router, Bot
 
 from openai import AsyncOpenAI
@@ -25,8 +26,9 @@ router = Router()
 async def cmd_start(message: Message, state: FSMContext, bot: Bot) -> None:
     if (delete_message_id := (await state.get_data()).get('message_id')): await bot.delete_message(chat_id=message.chat.id, message_id=delete_message_id)
     await state.clear()
-    message_log = False
-    delete_message = False
+    message_log = False #сообщение для логов в чат с пользователем в группе
+    delete_message = False #сообщение для удаления
+    generated_image_quality='standard'
     try:
         await UserService.check_user_rights(message.from_user.id)
         
@@ -34,31 +36,39 @@ async def cmd_start(message: Message, state: FSMContext, bot: Bot) -> None:
         
         temporary_user_data = await RoleManagmentService.get_temporary_user_data(message.from_user.id, 'all')
         
-        load_message = await message.answer(f'{Emojis.TIME} Генерация изображения...')
+        if temporary_user_data.img_model == 'dall-e-3-hd':
+            generated_image_quality='hd'
+            
         query_text = message.text[len('/img '):].strip()
         
         if query_text:
+            load_message = await message.answer(f'{Emojis.TIME} Генерация изображения...')
             try:
                 client = AsyncOpenAI(api_key=openai_api_key, base_url="https://api.proxyapi.ru/openai/v1")
+                
                 response = await client.images.generate(
-                    model=temporary_user_data.img_model,
+                    model=temporary_user_data.img_model if temporary_user_data.img_model != 'dall-e-3-hd' else 'dall-e-3',
                     prompt=query_text,
                     n=1,  # Количество изображений для генерации
                     size=temporary_user_data.quality_generated_image,  # Размер изображения
                     style='vivid',
-                    response_format='b64_json'
+                    response_format='b64_json',
+                    quality=generated_image_quality 
                 )
-                await bot.delete_message(chat_id=message.chat.id, message_id=load_message.message_id)
                 photo_file_path = f'downloads/generated_image{message.from_user.id}.png'
+                
                 with open(photo_file_path, 'wb') as photo_file:
                     photo_file.write(base64.b64decode(response.data[0].b64_json))
+                
                 photo = FSInputFile(photo_file_path)
-                message_log = await bot.send_photo(chat_id=message.chat.id, photo=photo, caption=f"Вы запросили изображение по запросу: {query_text}")
+                
+                await bot.delete_message(chat_id=message.chat.id, message_id=load_message.message_id)
+                message_log = await bot.send_photo(chat_id=message.chat.id, photo=photo, caption=f"<b>Вы запросили изображение по запросу</b>:\n{query_text}", parse_mode=ParseMode.HTML)
             except Exception as e:
                 message_log = await message.answer("Извините, произошла ошибка.")
                 logging.error(f"Ошибка при генерации изображения у пользователя {message.from_user.id}: {e}")
         else:
-            message_log = await message.reply("Пожалуйста, введите текст запроса после команды /img.")
+            message_log = await message.answer("Пожалуйста, введите текст запроса после команды /img.")
     except UserNotRegError:
         delete_message = await message.answer(f"{Emojis.ALLERT} Вы не зарегистрированы! {Emojis.ALLERT}\nДля регистрации введите команду /start", reply_markup=ReplyKeyboardRemove())
     except AccessDeniedError:
